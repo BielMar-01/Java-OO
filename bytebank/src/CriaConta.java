c41433895cdd327a99e67d3fbc68ecfe026621e8
Gabriel Marques <bielmarques922@gmail.com>
2022-06-30 18:24:12 -0300

    Java OO - Modulo 02 - Segunda Instancia
    Modulo 02 - Segunda Instancia

--- bytebank/src/CriaConta.java
+++ bytebank/src/CriaConta.java
@@ -3,7 +3,20 @@ public class CriaConta {
 
 	public static void main(String[] args) {
 		Conta primeiraConta = new Conta();
+		primeiraConta.titular = "Lucas";
 		primeiraConta.saldo = 200;
 		System.out.println(primeiraConta.saldo);
+		
+		primeiraConta.saldo += 100;
+		
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.titular = "Gabriel";
+		segundaConta.saldo = 100;
+		System.out.println(segundaConta.titular);
+		
+		System.out.println("Segunda conta é do " + primeiraConta.titular + " com saldo de " + primeiraConta.saldo);
+		System.out.println("Segunda conta é do " + segundaConta.titular + " com saldo de " + segundaConta.saldo);
 	}
 }
