28cbd580fa24d92a6d33dafa299cb94a583d98d4
Gabriel Marques <bielmarques922@gmail.com>
2022-07-04 17:36:19 -0300

    Java OO - Modulo 03 - Métodos com referência e mais retornos
    Modulo 03 - Métodos com referência e mais retornos

--- bytebank/src/TestaMetodo.java
+++ bytebank/src/TestaMetodo.java
@@ -10,5 +10,17 @@ public class TestaMetodo {
 		boolean conseguiuRetirar = contaDoUser.saca(20);
 		System.out.println(contaDoUser.saldo);
 		System.out.println(conseguiuRetirar);
+		
+		Conta contaDoUser2 = new Conta();
+		contaDoUser2.deposita(1000);
+		
+		boolean sucessoTranferencia = contaDoUser2.transfere(300, contaDoUser);
+		if(sucessoTranferencia) {
+			System.out.println("Transferencia feita com sucesso");
+		} else {
+			System.out.println("Erro ao tentar transferir, saldo insuficiente");
+		}
+		System.out.println(contaDoUser2.saldo);
+		System.out.println(contaDoUser.saldo);
 	}
 }
