e3c3234d7e849130497e0f29091304d8610b03d0
Gabriel Marques <bielmarques922@gmail.com>
2022-07-07 09:11:17 -0300

    Java OO - Modulo 05 - Getters e Setters de referência
    Modulo 05 - Getters e Setters de referência

--- bytebank-encapsulado/src/TestaGetESet.java
+++ bytebank-encapsulado/src/TestaGetESet.java
@@ -8,5 +8,24 @@ public class TestaGetESet {
 		conta.setAgencia(1);
 		System.out.println(conta.getNumero());
 		System.out.println(conta.getAgencia());
+		
+		Cliente gabriel = new Cliente();
+		//conta.titular = gabriel;
+		gabriel.setNome("Gabriel Marques");
+		
+		conta.setTitular(gabriel);
+		
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("Programador");
+		
+		// Agora em duas linhas:
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("Programador Java");
+		
+		// Chamando o mesmo dado:
+		System.out.println(titularDaConta);
+		System.out.println(gabriel);
+		System.out.println(conta.getTitular());
 	}
 }
