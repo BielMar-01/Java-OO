d9e749727fe658fa2de85e836f70155ce2e526fa
Gabriel Marques <bielmarques922@gmail.com>
2022-07-09 11:21:11 -0300

    Java OO - Modulo 06 - Static
    Modulo 06 - Static

--- bytebank-encapsulado/src/TestaValores.java
+++ bytebank-encapsulado/src/TestaValores.java
@@ -5,5 +5,9 @@ public class TestaValores {
 		Conta conta = new Conta(1337, 24226);
 		
 		System.out.println(conta.getAgencia());
+		
+		Conta conta2 = new Conta(1337, 16549);
+		
+		System.out.println(Conta.getTotal());
 	}
 }
